5ab26f386d4790dece4a7f77b9317dccebf9f1ef
alonravid1 <alonravid10@gmail.com>
2023-06-27 14:45:07 +0300

    added args to splity and validation accuracy

--- code/splity.py
+++ code/splity.py
@@ -1,30 +1,37 @@
-# Load the training data
-file_num = 1
+import sys
 
-with open(f"nn{file_num}.txt", "r") as data_file:
-    data = data_file.readlines()
+if __name__ == "__main__":
 
-data_size = 18000
-validation_size = 2000
-training_size = int(data_size * 0.8)
-test_size = int(data_size * 0.2)
+    file_num = 0
+    # get arguments
+    if len(sys.argv) > 1:
+        file_num = sys.argv[1]
 
-with open(f"training_set{file_num}.txt", "w") as training_file:
-    for line in data[:training_size]:
-        training_file.write(line)
+    # Load the training data
+    with open(f"nn{file_num}.txt", "r") as data_file:
+        data = data_file.readlines()
 
-with open(f"test_set{file_num}.txt", "w") as test_file:
-    for line in data[training_size:data_size]:
-        test_file.write(line)
+    data_size = 18000
+    validation_size = 2000
+    training_size = int(data_size * 0.8)
+    test_size = int(data_size * 0.2)
 
-with open(f"testnet{file_num}.txt", "w") as validation_file:
-    for line in data[data_size:]:
-        # get line until first tab
-        line = line.split("   ")[0]
-        validation_file.write(line + "\n")
+    with open(f"training_set{file_num}.txt", "w") as training_file:
+        for line in data[:training_size]:
+            training_file.write(line)
 
-with open(f"testnet{file_num}_actual_labels.txt", "w") as validation_file:
-    for line in data[data_size:]:
-        # get line until first tab
-        line = line.split("   ")[1]
-        validation_file.write(line)
+    with open(f"test_set{file_num}.txt", "w") as test_file:
+        for line in data[training_size:data_size]:
+            test_file.write(line)
+
+    with open(f"testnet{file_num}.txt", "w") as validation_file:
+        for line in data[data_size:]:
+            # get line until first tab
+            line = line.split("   ")[0]
+            validation_file.write(line + "\n")
+
+    with open(f"testnet{file_num}_actual_labels.txt", "w") as validation_file:
+        for line in data[data_size:]:
+            # get line until first tab
+            line = line.split("   ")[1]
+            validation_file.write(line)
--- code/validation_accuracy.py
+++ code/validation_accuracy.py
@@ -1,10 +1,17 @@
+import sys
 
 if __name__ == "__main__":
+    
+    file_num = 0
+    # get arguments
+    if len(sys.argv) > 1:
+        file_num = sys.argv[1]
+
     # open testnet0_actual_labels.txt and predictions0.txt, calculate accuracy
-    with open("testnet1_actual_labels.txt", "r") as actual_labels_file:
+    with open(f"testnet{file_num}_actual_labels.txt", "r") as actual_labels_file:
         actual_labels = actual_labels_file.readlines()
 
-    with open("predictions1.txt", "r") as predictions_file:
+    with open(f"predictions{file_num}.txt", "r") as predictions_file:
         predictions = predictions_file.readlines()
 
     correct = 0
